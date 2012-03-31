08bcf2b8bd97d106f74491690550a6f0fd5fc761
Romina Liuzzi <rliuzzi@gmail.com>
2012-03-31 14:30:47 +0200

    Extract onClickListener instantiation outside the onCreate method.Rename variables.

--- caloriecalc/src/com/caloriecalc/presentation/EjercicioActualActivity.java
+++ caloriecalc/src/com/caloriecalc/presentation/EjercicioActualActivity.java
@@ -19,28 +19,39 @@ import com.caloriecalc.beans.Ejercicio.TipoEjercicio;
 import com.caloriecalc.lao.LaoEjercicio;
 import com.caloriecalc.lao.LaoProgreso;
 
+/**
+ * @author Romina
+ *
+ */
+/**
+ * @author Romina
+ *
+ */
 public class EjercicioActualActivity extends Activity {
-	
 
 	private Button btnFinalizar;
-	
+
 	private TextView lblLatitud;
 	private TextView lblLongitud;
 	private TextView lblPrecision;
 	private TextView lblEstado;
-	
+
 	private LaoProgreso laoProgreso;
 	private LaoEjercicio laoEjercicio;
 	private Ejercicio ejercicio;
 
-	private LocationManager locManager;
-	
+	private LocationManager locationManager;
 
-	// Nos registramos para recibir actualizaciones de la posición
-	private LocationListener locListener = new LocationListener() {
+	
+	/**
+	 * Registrarmos un LocationListener para recibir actualizaciones de la posicion
+	 */
+	private LocationListener locationListener = new LocationListener() {
+		
 		public void onLocationChanged(Location location) {
 			mostrarPosicion(location);
-			laoProgreso.guardarProgreso(ejercicio.getId(), location.getLatitude(), location.getLongitude());
+			laoProgreso.guardarProgreso(ejercicio.getId(),
+					location.getLatitude(), location.getLongitude());
 		}
 
 		public void onProviderDisabled(String provider) {
@@ -51,84 +62,89 @@ public class EjercicioActualActivity extends Activity {
 			lblEstado.setText("Provider ON ");
 		}
 
-		public void onStatusChanged(String provider, int status,
-				Bundle extras) {
+		public void onStatusChanged(String provider, int status, Bundle extras) {
 			Log.i("", "Provider Status: " + status);
 			lblEstado.setText("Provider Status: " + status);
 		}
 	};
-
 	
+	
+	private OnClickListener finalizar =  new OnClickListener() {
 
+		public void onClick(View v) {
+
+			locationManager.removeUpdates(locationListener);
+
+			laoProgreso.finalizarEjercicio(ejercicio);
+
+			Intent i = new Intent(EjercicioActualActivity.this,
+					CalorieCalcResult.class);
+
+			// send ejercicioId
+			i.putExtra("ejercicioId", ejercicio.getId());
+
+			startActivity(i);
+		}
+
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ejercicio_actual);
-		
+
 		laoProgreso = new LaoProgreso(EjercicioActualActivity.this);
-		laoEjercicio = new LaoEjercicio(EjercicioActualActivity.this);
-		
-		
-        btnFinalizar = (Button)findViewById(R.id.BtnDesactivar);
-		
+		laoEjercicio = new LaoEjercicio(EjercicioActualActivity.this);		
+
 		lblLatitud = (TextView) findViewById(R.id.LblPosLatitud);
 		lblLongitud = (TextView) findViewById(R.id.LblPosLongitud);
 		lblPrecision = (TextView) findViewById(R.id.LblPosPrecision);
 		lblEstado = (TextView) findViewById(R.id.LblEstado);
-		       
-		btnFinalizar.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-				
-				locManager.removeUpdates(locListener);
-				
-				laoProgreso.finalizarEjercicio(ejercicio);
-				
-				Intent i = new Intent(EjercicioActualActivity.this,
-						CalorieCalcResult.class);
-				
-				//send ejercicioId
-				i.putExtra("ejercicioId", ejercicio.getId());
-				
-				startActivity(i);
-			}
 
-		});
-		
+		btnFinalizar = (Button) findViewById(R.id.BtnDesactivar);
+		btnFinalizar.setOnClickListener(finalizar);
 
-		//Leermos el intent que llamo a esta activdad para averiguar el valor del tipoEjercicio
-		//Devuvele -1 si no se ha inicializado en la llamada.
-		
+		// Leermos el intent que llamo a esta activdad para averiguar el valor
+		// del tipoEjercicio
+		// Devuvele -1 si no se ha inicializado en la llamada.
+
 		Intent i = getIntent();
-		TipoEjercicio tipoEjercicio = TipoEjercicio.values()[i.getIntExtra("ejercicio", -1)];
-		
+		TipoEjercicio tipoEjercicio = TipoEjercicio.values()[i.getIntExtra(
+				"ejercicio", -1)];
+
 		ejercicio = laoEjercicio.crearEjercicio(tipoEjercicio, 123);
-		
+
 		comenzarLocalizacion();
 
 	}
 
+	/**
+	 * @author Romina
+	 */
 	private void comenzarLocalizacion() {
 		// Obtenemos una referencia al LocationManager
-		locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		
-		Location loc = locManager
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		Location loc = locationManager
 				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		
-		
 
 		// Mostramos la última posición conocida
 		mostrarPosicion(loc);
-		
+
 		if (loc != null) {
-			laoProgreso.guardarProgreso(ejercicio.getId(), loc.getLatitude(), loc.getLongitude());
+			laoProgreso.guardarProgreso(ejercicio.getId(), loc.getLatitude(),
+					loc.getLongitude());
 		}
 
-		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0, locListener);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000,
+				0, locationListener);
 	}
 
+	/**
+	 * @param loc
+	 * @author Romina
+	 */
 	private void mostrarPosicion(Location loc) {
 		if (loc != null) {
 			lblLatitud.setText("Latitud: " + String.valueOf(loc.getLatitude()));
@@ -145,8 +161,5 @@ public class EjercicioActualActivity extends Activity {
 			lblPrecision.setText("Precision: (sin_datos)");
 		}
 	}
-	
-	
 
-	
 }
